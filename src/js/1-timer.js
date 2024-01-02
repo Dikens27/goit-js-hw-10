6c76bac60a8b06ce360d2a333cfda7d6969864c2
Dikens27 <kar.karovich321@gmail.com>
2024-01-02 20:28:28 +0200

    Update tamir.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,6 +3,10 @@ import "flatpickr/dist/flatpickr.min.css";
 
 const input = document.querySelector("#datetime-picker");
 const button = document.querySelector("button");
+const daysTimer = document.querySelector("[data-days]"); 
+const hoursTimer = document.querySelector("[data-hours]");
+const minutesTimer = document.querySelector("[data-minutes]");
+const secondsTimer = document.querySelector("[data-seconds]");
 
 let userSelectedDate = "";
 
@@ -13,17 +17,72 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     if (selectedDates[0] < options.defaultDate) {
-        window.alert("Please choose a date in the future");
-        button.setAttribute("disabled", true);
+      window.alert("Please choose a date in the future");
+      button.setAttribute("disabled", true);
     } else {
-        button.removeAttribute("disabled")
-        userSelectedDate = selectedDates[0];
-      }
+      button.removeAttribute("disabled")
+      userSelectedDate = selectedDates[0];
+    }
     
     console.log(selectedDates[0]);
-    },
+  }
    
-};
+}
 
-const datePicker = flatpickr(input, options);
+flatpickr(input, options);
+
+
+
+
+
+
+
+
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = pad(Math.floor(ms / day));
+  const hours = pad(Math.floor((ms % day) / hour));
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds }
+}
+
+
+button.addEventListener('click', () => {
+  const currentDateTime = new Date().getTime(1000);
+  const selectedDateTime = userSelectedDate.getTime();
+
+  setInterval(() => {
+    let different = selectedDateTime - currentDateTime - 1000;
+
+    const result = convertMs(different);
+
+    daysTimer.textContent = `${result.days}`;
+    hoursTimer.textContent = `${result.hours}`;
+    minutesTimer.textContent = `${result.minutes}`;
+    secondsTimer.textContent = `${result.seconds}`;
+  }, 1000);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
