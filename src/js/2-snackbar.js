d943384a73b545babcc4639197c84c28a5204720
Dikens27 <kar.karovich321@gmail.com>
2024-01-05 20:20:55 +0200

    snackbar1

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,31 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector(".form");
+const inputDeley = document.querySelector('input[name="delay"]');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const delay = parseInt(inputDeley.value, 10);
+    const state = form.elements.state.value;
+
+    const notificationPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === "fulfilled") {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    });
+
+    notificationPromise
+        .then((delay) => {
+            console.log(`✅ Fulfilled promise in ${delay}ms`);
+        })
+        .catch((delay) => {
+            console.log(`❌ Rejected promise in ${delay}ms`);
+        });
+});
+
